23f97b0483a2960de851cab1b3543bc048455693
o310yusuke <yusuke310@gmail.com>
2015-12-31 22:25:06 +0900

    Additional learning code. Ref. <http://kokuzawa.github.io/blog/2013/12/08/javafx-on-intellij-idea/>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * 参考URL：
+ * http://kokuzawa.github.io/blog/2013/12/08/javafx-on-intellij-idea/
+ */
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button;
+
+    public void click() {
+        label.setText("ボタンをクリックしたよ！");
+    }
 }
